488ce0f94e055ddd1ec2c3f77793b4bf2e2550aa
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2022-05-05 15:13:36 +0900

    ネットワーク周りから、EC2 インスタンスと RDS Aurora の構築までを自動化した

---
+++ full-stack/dev/variables.tf
@@ -0,0 +1,8 @@
+variable "username" {
+  description = "The username for the DB master user"
+  type        = string
+}
+variable "password" {
+  description = "The password for the DB master user"
+  type        = string
+}
